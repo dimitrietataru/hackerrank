703554ce71580acb320743b5690cb5bf5d339309
Tataru Dimitrie <dimitriettr@gmail.com>
2018-04-08 00:34:21 +0300

    Implementation 61-66

--- Algorithms/02. Implementation/64 - Matrix layer rotation/Main.java
+++ Algorithms/02. Implementation/64 - Matrix layer rotation/Main.java
@@ -12,73 +12,76 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("input.txt");
         Scanner scan = new Scanner(file);
-
-        int n = scan.nextInt();
-        int m = scan.nextInt();
-        int rotations = scan.nextInt();
-        int[][] matrix = new int[n][m];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                matrix[i][j] = scan.nextInt();
+        {
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+            int rotations = scan.nextInt();
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    matrix[i][j] = scan.nextInt();
+                }
             }
+            matrixRotation(matrix, n, m, rotations);
         }
         scan.close();
-
-        matrixRotation(matrix, n, m, rotations);
     }
 
-    private static void matrixRotation(int[][] matrix, int n, int m, int r) {
-        int totalLayers = (n >= m) ? (m / 2) : (n / 2);
-        for (int layer = 0; layer < totalLayers; ++layer) {
-            List<Integer> inputLayer = getLayer(matrix, n, m, layer);
-            List<Integer> rotatedLayer = rotateLayer(inputLayer, r);
-            updateMatrix(matrix, n, m, rotatedLayer, layer);
+    private static void matrixRotation(int[][] matrix, int n, int m, int rotations) {
+        int layers = (n >= m) ? (m / 2) : (n / 2);
+
+        for (int l = 0; l < layers; ++l) {
+            List<Integer> layerAsList = getLayer(matrix, n, m, l);
+            List<Integer> rotatedLayer = rotateLayer(layerAsList, rotations);
+            updateMatrix(matrix, n, m, l, rotatedLayer);
         }
         printMatrix(matrix, n, m);
     }
 
-    private static List<Integer> getLayer(int[][] matrix, int n, int m, int layer) {
-        List<Integer> layerList = new ArrayList<>();
-        for (int i = layer; i < m - layer; ++i) {
-            layerList.add(matrix[layer][i]);
+    private static List<Integer> getLayer(int[][] matrix, int n, int m, int l) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = l; i < m - l; ++i) {
+            result.add(matrix[l][i]);
         }
-        for (int i = (layer + 1); i < (n - layer - 1); ++i) {
-            layerList.add(matrix[i][m - layer - 1]);
+        for (int i = (l + 1); i < (n - l - 1); ++i) {
+            result.add(matrix[i][m - l - 1]);
         }
-        for (int i = (m - layer - 1); i >= layer; --i) {
-            layerList.add(matrix[n - layer - 1][i]);
+        for (int i = (m - l - 1); i >= l; --i) {
+            result.add(matrix[n - l - 1][i]);
         }
-        for (int i = (n - layer - 2); i > layer; --i) {
-            layerList.add(matrix[i][layer]);
+        for (int i = (n - l - 2); i > l; --i) {
+            result.add(matrix[i][l]);
         }
 
-        return layerList;
+        return result;
     }
 
     private static List<Integer> rotateLayer(List<Integer> layer, int r) {
         r = (layer.size() + r) % layer.size();
 
-        List<Integer> rotatedLayer = new ArrayList<>(layer.size());
+        List<Integer> result = new ArrayList<>(layer.size());
         for (int i = 0; i < layer.size(); ++i) {
-            rotatedLayer.add(i, layer.get((i + layer.size() + r) % layer.size()));
+            int indexR = (i + r) % layer.size();
+            result.add(layer.get(indexR));
         }
 
-        return rotatedLayer;
+        return result;
     }
-
-    private static void updateMatrix(int[][] matrix, int n, int m, List<Integer> elements, int layer) {
+    
+    private static void updateMatrix(int[][] matrix, int n, int m, int l, List<Integer> elements) {
         int index = 0;
-        for (int i = layer; i < m - layer; ++i) {
-            matrix[layer][i] = elements.get(index++);
+        for (int i = l; i < m - l; ++i) {
+            matrix[l][i] = elements.get(index++);
         }
-        for (int i = (layer + 1); i < (n - layer - 1); ++i) {
-            matrix[i][m - layer - 1] = elements.get(index++);
+        for (int i = (l + 1); i < (n - l - 1); ++i) {
+            matrix[i][m - l - 1] = elements.get(index++);
         }
-        for (int i = (m - layer - 1); i >= layer; --i) {
-            matrix[n - layer - 1][i] = elements.get(index++);
+        for (int i = (m - l - 1); i >= l; --i) {
+            matrix[n - l - 1][i] = elements.get(index++);
         }
-        for (int i = (n - layer - 2); i > layer; --i) {
-            matrix[i][layer] = elements.get(index++);
+        for (int i = (n - l - 2); i > l; --i) {
+            matrix[i][l] = elements.get(index++);
         }
     }
 
