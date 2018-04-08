93935f2d107fcfa8e5ac9635bbddcd8cf4a3feca
Tataru Dimitrie <dimitriettr@gmail.com>
2018-04-09 00:13:57 +0300

    C# solutions

---
+++ 30 days of code/__ C#/Day 02 - Operators.cs
@@ -0,0 +1,29 @@
+﻿/**
+ *              OPERATORS | C#
+ * Domain       30 DAYS OF CODE
+ * Sub domain   DAY 02
+ */
+
+using System;
+using System.Globalization;
+using System.IO;
+
+class Program
+{
+    //static TextReader read = Console.In;
+    static StreamReader read = new StreamReader("input.txt");
+
+    static void Main(string[] args)
+    {
+        double mealCost = Double.Parse(read.ReadLine(), CultureInfo.InvariantCulture);
+        int tipPercent = int.Parse(read.ReadLine());
+        int taxPercent = int.Parse(read.ReadLine());
+        read.Close();
+
+        double tip = (mealCost * tipPercent) / 100;
+        double tax = (mealCost * taxPercent) / 100;
+        int totalCost = (int)Math.Round(mealCost + tip + tax);
+
+        Console.WriteLine("The total meal cost is " + totalCost + " dollars.");
+    }
+}
---
+++ 30 days of code/__ C#/Day 19 - Interfaces.cs
@@ -0,0 +1,44 @@
+﻿/**
+ *              INTERFACES | C#
+ * Domain       30 DAYS OF CODE
+ * Sub domain   DAY 19
+ */
+
+using System;
+using System.IO;
+
+class Program
+{
+    //static TextReader read = Console.In;
+    static StreamReader read = new StreamReader("input.txt");
+
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(read.ReadLine());
+        read.Close();
+
+        IAdvancedArithmetic calculator = new Calculator();
+        int sum = calculator.DivisorSum(n);
+        Console.WriteLine("I implemented: " + calculator.GetType().GetInterfaces()[0].Name);
+        Console.WriteLine(sum);
+    }
+}
+
+interface IAdvancedArithmetic
+{
+    int DivisorSum(int n);
+}
+
+class Calculator : IAdvancedArithmetic
+{
+    public int DivisorSum(int n)
+    {
+        int sum = n;
+        for (int i = 1; i <= n / 2; ++i)
+        {
+            sum += (n % i == 0) ? i : 0;
+        }
+
+        return sum;
+    }
+}
