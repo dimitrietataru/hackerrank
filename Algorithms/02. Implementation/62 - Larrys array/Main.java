e9c41f3cf146464ba8d297d72cdf7518239c5381
Tataru Dimitrie <dimitriettr@gmail.com>
2018-04-09 00:20:22 +0300

    Algorithms - Implementation 61-66

---
+++ Algorithms/02. Implementation/62 - Larrys array/Main.java
@@ -0,0 +1,59 @@
+/**
+ *              62 | LARRY'S ARRAY
+ * Domain       ALGORITHMS
+ * Sub domain   IMPLEMENTATION
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner scan = new Scanner(file);
+        {
+            int testCases = scan.nextInt();
+            while (testCases-- > 0) {
+                int[] array = new int[scan.nextInt()];
+                for (int i = 0; i < array.length; ++i) {
+                    array[i] = scan.nextInt();
+                }
+                larrysArray(array);
+            }
+        }
+        scan.close();
+    }
+
+    private static void larrysArray(int[] array) {
+        int count = 0;
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; ++i) {
+                if (array[i] > array[i + 1]) {
+                    count += smallBubbleSort(array, i, i + 2);
+                    isSorted = false;
+                }
+            }
+        }
+
+        System.out.println((count % 2 == 0) ? "YES" : "NO");
+    }
+
+    private static int smallBubbleSort(int[] array, int start, int end) {
+        int count = 0;
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = start; i < end; ++i) {
+                if (array[i] > array[start + 1]) {
+                    array[i] = array[i] + array[i + 1] - (array[i + 1] = array[i]);
+                    count++;
+                    isSorted = false;
+                }
+            }
+        }
+        return count;
+    }
+}
