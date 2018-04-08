47137e78049e009dacf9a04bacd223cde892f135
Tataru Dimitrie <dimitriettr@gmail.com>
2018-04-08 21:06:16 +0300

    Java - Exception handling

--- Java/06. Exception handling/01 - Java exception handling (Try-catch)/Main.java
+++ Java/06. Exception handling/01 - Java exception handling (Try-catch)/Main.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         File file = new File("input.txt");
-
         try {
             Scanner scan = new Scanner(file);
             int x = scan.nextInt();
