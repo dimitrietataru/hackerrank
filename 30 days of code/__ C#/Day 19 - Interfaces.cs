27b4c76bc56172df94f5211c97caf04a9eba565c
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-04 15:36:40 +0200

    C# - Day 19

--- 30 days of code/__ C#/Day 19 - Interfaces.cs
+++ 30 days of code/__ C#/Day 19 - Interfaces.cs
@@ -9,16 +9,15 @@ using System.IO;
 
 class Program
 {
-    //static TextReader read = Console.In;
-    static StreamReader read = new StreamReader("input.txt");
-
     static void Main(string[] args)
     {
-        int n = Int32.Parse(read.ReadLine());
-        read.Close();
+        var reader = new InputReader(false);
+        int n = reader.NextInt;
+        reader.Close();
 
         IAdvancedArithmetic calculator = new Calculator();
         int sum = calculator.DivisorSum(n);
+
         Console.WriteLine("I implemented: " + calculator.GetType().GetInterfaces()[0].Name);
         Console.WriteLine(sum);
     }
@@ -34,6 +33,7 @@ class Calculator : IAdvancedArithmetic
     public int DivisorSum(int n)
     {
         int sum = n;
+
         for (int i = 1; i <= n / 2; ++i)
         {
             sum += (n % i == 0) ? i : 0;
@@ -41,4 +41,20 @@ class Calculator : IAdvancedArithmetic
 
         return sum;
     }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public string NextLine => input.ReadLine();
+
+    public int NextInt => int.Parse(NextLine);
+
+    public void Close() => input.Close();
 }
