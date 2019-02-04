306b5302b2790156773d42b8f5fe932b9284437e
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-04 13:00:54 +0200

    C# - Day 07

--- 30 days of code/__ C#/Day 07 - Arrays.cs
+++ 30 days of code/__ C#/Day 07 - Arrays.cs
@@ -9,19 +9,33 @@ using System.IO;
 
 class Program
 {
-    //static TextReader read = Console.In;
-    static StreamReader read = new StreamReader("input.txt");
-
     static void Main(string[] args)
     {
-        int n = int.Parse(read.ReadLine());
-        string[] line = read.ReadLine().Split(' ');
-        int[] array = Array.ConvertAll(line, int.Parse);
-        read.Close();
+        var reader = new InputReader(false);
+        int n = reader.NextInt;
+        int[] array = reader.NextArrInt;
+        reader.Close();
 
         for (int i = n - 1; i >= 0; --i)
         {
             Console.Write("{0} ", array[i]);
         }
     }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public string NextLine => input.ReadLine();
+
+    public int NextInt => int.Parse(NextLine);
+    public int[] NextArrInt => Array.ConvertAll(NextLine.Split(), int.Parse);
+
+    public void Close() => input.Close();
 }
