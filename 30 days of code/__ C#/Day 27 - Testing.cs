1f812ad2fb3616b729b5ccc414d4908b77cf32fe
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-04 16:46:51 +0200

    C# - Day 27

---
+++ 30 days of code/__ C#/Day 27 - Testing.cs
@@ -0,0 +1,25 @@
+﻿/**
+ *              TESTING | C#
+ * Domain       30 DAYS OF CODE
+ * Sub domain   DAY 27
+ */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("5");
+        Console.WriteLine("5 3");
+        Console.WriteLine("0 -1 1 100 101");
+        Console.WriteLine("6 4");
+        Console.WriteLine("0 -1 1 100 101 102");
+        Console.WriteLine("7 3");
+        Console.WriteLine("0 -1 1 100 101 102 103");
+        Console.WriteLine("8 4");
+        Console.WriteLine("0 -1 1 100 101 102 103 104");
+        Console.WriteLine("9 3");
+        Console.WriteLine("0 -1 1 100 101 102 104 105 106");
+    }
+}
