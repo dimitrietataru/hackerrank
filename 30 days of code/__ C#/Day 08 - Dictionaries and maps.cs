3bb7d68f708d1c1d9764e0279121918c6918b067
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-04 13:00:59 +0200

    C# - Day 08

--- 30 days of code/__ C#/Day 08 - Dictionaries and maps.cs
+++ 30 days of code/__ C#/Day 08 - Dictionaries and maps.cs
@@ -10,21 +10,20 @@ using System.IO;
 
 class Program
 {
-    //static TextReader read = Console.In;
-    static StreamReader read = new StreamReader("input.txt");
-
     static void Main(string[] args)
     {
-        int n = int.Parse(read.ReadLine());
-        Dictionary<string, int> phoneBook = new Dictionary<string, int>(n);
+        var reader = new InputReader(false);
+        int n = reader.NextInt;
+        var phoneBook = new Dictionary<string, int>(n);
+
         for (int i = 0; i < n; ++i)
         {
-            string[] line = read.ReadLine().Split(' ');
+            var line = reader.NextArrString;
             phoneBook.Add(line[0], int.Parse(line[1]));
         }
 
-        string s = null;
-        while ((s = read.ReadLine()) != null)
+        string s;
+        while ((s = reader.NextString) != null)
         {
             if (phoneBook.ContainsKey(s))
             {
@@ -36,6 +35,26 @@ class Program
             }
         }
 
-        read.Close();
+        reader.Close();
     }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public string NextLine => input.ReadLine();
+
+    public int NextInt => int.Parse(NextLine);
+
+    public string NextString => NextLine;
+
+    public string[] NextArrString => NextLine.Split();
+
+    public void Close() => input.Close();
 }
