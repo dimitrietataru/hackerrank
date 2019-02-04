c3fea2f1155e8833ad2f12b1d4f0861c90b35153
Tataru Dimitrie <dimitriettr@gmail.com>
2019-02-04 23:57:13 +0200

    Algorithms - Warmup - Simple array sum

---
+++ Algorithms C#/Algorithms/01. Warmup/Simple array sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reader = new InputReader(true);
+
+        int n = reader.NextInt;
+        int[] array = reader.NextArrInt;
+
+        Console.WriteLine(array.Sum());
+
+        reader.Close();
+    }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public int NextInt => int.Parse(input.ReadLine());
+    public int[] NextArrInt => Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+    public void Close() => input.Close();
+}
