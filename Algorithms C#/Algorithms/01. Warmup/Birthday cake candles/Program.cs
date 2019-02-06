264200ac9a75a3a4045f6d06dfca125119cd57b6
Tataru Dimitrie <dimitriettr@gmail.com>
2019-02-06 23:22:17 +0200

    Algorithms - Warmup - Birthday cake candles

---
+++ Algorithms C#/Algorithms/01. Warmup/Birthday cake candles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reader = new InputReader(true);
+        int n = reader.NextInt;
+        int[] array = reader.NextArrInt;
+        reader.Close();
+
+        Solve(array);
+    }
+
+    static void Solve(int[] candles)
+    {
+        int max = candles.Max();
+        var result = candles.Where(x => x == max).Count();
+
+        Console.WriteLine(result);
+    }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public int NextInt => int.Parse(input.ReadLine());
+
+    public int[] NextArrInt => Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+    public void Close() => input.Close();
+}
