fea14bcdbf643c24395ef8cabe41597520b4dbc9
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-26 14:54:24 +0200

    Algorithms - Strings - Pangrams

---
+++ Algorithms C#/Algorithms/03. Strings/Pangrams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reader = new InputReader(true);
+        string input = reader.NextString;
+        reader.Close();
+
+        Solve(input);
+    }
+
+    static void Solve(string input)
+    {
+        var letters = new SortedSet<char>(input.Trim().Replace(" ", "").ToLower());
+
+        Console.WriteLine(letters.Count == 26 ? "pangram" : "not pangram");
+    }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public string NextString => input.ReadLine();
+
+    public void Close() => input.Close();
+}
