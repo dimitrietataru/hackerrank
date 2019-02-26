ca2c40086e70169b7050c86a436a4701bb3c5d55
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-26 17:11:46 +0200

    Algorithms - Strings - Gemstones

---
+++ Algorithms C#/Algorithms/03. Strings/Gemstones/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reader = new InputReader(true);
+
+        var gems = new List<char[]>();
+        int n = reader.NextInt;
+        
+        while (n-- > 0)
+        {
+            gems.Add(reader.NextString.ToCharArray());
+        }
+
+        reader.Close();
+
+        Solve(gems);
+    }
+
+    static void Solve(List<char[]> gems)
+    {
+        var result = gems.Aggregate((g1, g2) => g1.Intersect(g2).ToArray());
+
+        Console.WriteLine(result.Length);
+    }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public int NextInt => int.Parse(input.ReadLine());
+
+    public string NextString => input.ReadLine();
+
+    public void Close() => input.Close();
+}
