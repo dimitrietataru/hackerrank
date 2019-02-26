34387d69638a393e9b7081fa2b70aeedc5f0eda7
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-02-26 14:26:09 +0200

    Algorithms - Strings - Caesar cipher

---
+++ Algorithms C#/Algorithms/03. Strings/Caesar cipher/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reader = new InputReader(true);
+
+        int n = reader.NextInt;
+        string input = reader.NextString;
+        int r = reader.NextInt;
+
+        reader.Close();
+
+        Solve(input, r);
+    }
+
+    static void Solve(string input, int r)
+    {
+        var result = input
+            .ToCharArray()
+            .Select(c =>
+            {
+                if (c >= 65 && c <= 90)
+                {
+                    return (char)((c - 'A' + r) % 26 + 'A');
+                }
+
+                if (c >= 97 && c <= 122)
+                {
+                    return (char)((c - 'a' + r) % 26 + 'a');
+                }
+
+                return c;
+            });
+
+        Console.WriteLine(string.Join("", result));
+    }
+}
+
+class InputReader
+{
+    private TextReader input;
+
+    public InputReader(bool fromFile)
+    {
+        input = fromFile ? new StreamReader("input.txt") : Console.In;
+    }
+
+    public int NextInt => int.Parse(input.ReadLine());
+
+    public string NextString => input.ReadLine();
+
+    public void Close() => input.Close();
+}
